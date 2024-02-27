99108b904dded497ab37821a09fffef95fc48406
cwdatlas <ascott@carroll.edu>
2024-02-27 01:04:46 -0700

    deleted launchedEffect, it did not fix status updating

--- src/main/kotlin/ui/ActionColumn.kt
+++ src/main/kotlin/ui/ActionColumn.kt
@@ -12,69 +12,64 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import data.models.AppState
 import data.models.ButtonActions
-import data.models.InitParts
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import utils.handCursor
 
 @Composable
-fun Title(infoPanel: (selectedButton:ButtonActions) -> Unit){
-    val scope = rememberCoroutineScope()
-    LaunchedEffect(AppState.rocket){
-        scope.launch {
-            Column(
-                modifier = Modifier.fillMaxWidth(0.25f).fillMaxHeight().padding(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text("Rocket Builder", Modifier.padding(1.dp))
+fun Title(infoPanel: (selectedButton: ButtonActions) -> Unit) {
 
-                // Generate all buttons stated in the ActionButtons
-                ButtonActions.entries.forEach{
-                    TextButton(
-                        onClick = {
-                            infoPanel(it)
-                        }
-                    ){
-                        Text(
-                            it.toString(),
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black,
-                            modifier = Modifier.pointerHoverIcon(handCursor())
-                        )
-                    }
+    Column(
+        modifier = Modifier.fillMaxWidth(0.25f).fillMaxHeight().padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Rocket Builder", Modifier.padding(1.dp))
+
+        // Generate all buttons stated in the ActionButtons
+        ButtonActions.entries.forEach {
+            TextButton(
+                onClick = {
+                    infoPanel(it)
                 }
-                Spacer(modifier = Modifier.padding(10.dp))
+            ) {
                 Text(
-                    "Rocket Statistics",
+                    it.toString(),
+                    fontWeight = FontWeight.Bold,
                     color = Color.Black,
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    if(AppState.rocket.getTWR().isNaN()) "TWR: 0" else "TWR: ${AppState.rocket.getTWR()}",
-                    color = Color.Black,
-                    fontWeight = FontWeight.Light
-                )
-                Text(
-                    "ISP: ${AppState.rocket.getISP()}",
-                    color = Color.Black,
-                    fontWeight = FontWeight.Light
-                )
-                Text(
-                    "Engine Number: ${AppState.rocket.engineNumber}",
-                    color = Color.Black,
-                    fontWeight = FontWeight.Light
-                )
-                Text(
-                    "Launch Attempts: ${AppState.numberOfLaunches}",
-                    color = Color.Black,
-                    fontWeight = FontWeight.Light
-                )
-                Text(
-                    "Previous DeltaV: ${AppState.previousDeltaV}",
-                    color = Color.Black,
-                    fontWeight = FontWeight.Light
+                    modifier = Modifier.pointerHoverIcon(handCursor())
                 )
             }
         }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text(
+            "Rocket Statistics",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            if (AppState.rocket.getTWR().isNaN()) "TWR: 0" else "TWR: ${AppState.rocket.getTWR()}",
+            color = Color.Black,
+            fontWeight = FontWeight.Light
+        )
+        Text(
+            "ISP: ${AppState.rocket.getISP()}",
+            color = Color.Black,
+            fontWeight = FontWeight.Light
+        )
+        Text(
+            "Engine Number: ${AppState.rocket.engineNumber}",
+            color = Color.Black,
+            fontWeight = FontWeight.Light
+        )
+        Text(
+            "Launch Attempts: ${AppState.numberOfLaunches}",
+            color = Color.Black,
+            fontWeight = FontWeight.Light
+        )
+        Text(
+            "Previous DeltaV: ${AppState.previousDeltaV}",
+            color = Color.Black,
+            fontWeight = FontWeight.Light
+        )
     }
-
 }
